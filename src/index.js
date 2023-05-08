67e108f4b6c5ce3d3c363dc044cb11732db036c2
wendal2841 <wendal@bk.ru>
2023-05-08 17:07:26 +0300

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './teleporthq/style.css'
+import Home from './teleporthq/pages/home'
+
+const App = () => {
+  return (
+    <Home/>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
 
